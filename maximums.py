7b6f46d8afa9d628de4843bc48fab76bfca204b8
pedrofarr <pedrinifarrusini@gmail.com>
2025-09-15 12:42:45 -0300

    Add files via upload

--- maximums.py
+++ maximums.py
@@ -1,10 +1,11 @@
 def max_of_two(x, y):
-    """Given x and y, that are 2 numbers, return the biggest number."""
-    return x if x > y else y
-
+    if x > y:
+        return x
+    else:
+        return y
 
 def max_of_three(x, y, z):
-    """Given x, y and z, that are 3 numbers, return the biggest number of the three."""
-    return max_of_two(x, max_of_two(y, z)) 
-  #max_of_two()
-  #max_of_three()
+    return max_of_two(x, max_of_two(y, z))
+
+#max_of_two()
+#max_of_three()
